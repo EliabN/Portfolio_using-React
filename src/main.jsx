1b199009e6c0a7174c1c59faf0a14933c5efd45b
eliabn <30064396@tafe.wa.edu.au>
2023-11-04 00:37:25 +1030

    Created main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// Import client-side ReactDOM from the react-dom library
+import ReactDOM from 'react-dom/client'
+// Import main App component
+import App from './App.jsx'
+
+// Render the App component into the root element using createRoot method to 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // Render App component into the root element
+  <App />
+)
