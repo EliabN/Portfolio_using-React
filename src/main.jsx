526732665ec8d295a181a41102990b2bcbe0f277
eliabn <30064396@tafe.wa.edu.au>
2023-11-04 22:36:49 +1030

    Import and add index.ccs

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@
 import ReactDOM from 'react-dom/client'
 // Import main App component
 import App from './App.jsx'
+// Import css styling
+import './index.css'
 
 // Render the App component into the root element using createRoot method to 
 ReactDOM.createRoot(document.getElementById('root')).render(
